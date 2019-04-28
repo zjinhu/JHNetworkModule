bdc3b2c66d2b757c5a185e9eb4137cb4a557769d
jinhu zhang <814030966@qq.com>
2019-04-28 16:16:58 +0800

    release

---
+++ JHNetworkModule/JHNetworkModule/Class/JHNetworkModule.h
@@ -0,0 +1,14 @@
+//
+//  JHNetworkModule.h
+//  JHNetworkModule
+//
+//  Created by 狄烨 . on 2019/4/28.
+//  Copyright © 2019 HU. All rights reserved.
+//
+
+#ifndef JHNetworkModule_h
+#define JHNetworkModule_h
+#import "JHNetworking.h"
+#import "JHBaseRequest.h"
+#import "JHBaseModel.h"
+#endif /* JHNetworkModule_h */
